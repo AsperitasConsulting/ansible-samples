608de8ac340227fce6c0f9a3dcb7a2212a73a580
Derek C. Ashmore <derek.ashmore@asperitas.consulting>
2025-02-06 02:51:03 -0500

    Fix the resource group reference for the vnet

--- infrastructure/azure/vm_setup/vm_setup_vnet.tf
+++ infrastructure/azure/vm_setup/vm_setup_vnet.tf
@@ -2,14 +2,14 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "vm-network"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
 }
 
 # Create a subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "vm-subnet"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.resource_group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
