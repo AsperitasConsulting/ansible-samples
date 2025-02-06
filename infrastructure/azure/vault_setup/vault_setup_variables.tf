ccfac076df58b3c65add0ff44ea6d0458b622cbd
Derek C. Ashmore <derek.ashmore@dvtconsulting.com>
2025-02-06 00:49:43 -0500

    Add Azure Vault IaC with workflow (#2)
    Co-authored-by: Derek C. Ashmore <derek.ashmore@asperitas.consulting>

---
+++ infrastructure/azure/vault_setup/vault_setup_variables.tf
@@ -0,0 +1,16 @@
+
+variable location {
+    type = string
+    description = "Azure region"
+}
+
+variable resource_group_name {
+    type = string
+    description = "Resource group name"
+}
+
+variable key_vault_name {
+    type = string
+    description = "Key vault name"
+}
+
