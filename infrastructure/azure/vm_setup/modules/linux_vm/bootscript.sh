cba95de3b742a52a8fb4c3e492ffb8de1756aa46
Derek C. Ashmore <derek.ashmore@asperitas.consulting>
2025-02-08 08:42:59 -0500

    Add boot script to linux vm

--- infrastructure/azure/vm_setup/modules/linux_vm/bootscript.sh
+++ infrastructure/azure/vm_setup/modules/linux_vm/bootscript.sh
@@ -2,3 +2,5 @@
 
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3-future python3-setuptools
+sudo pip3 install 'typing-extensions>=3.7.4' 'future>=0.18.2'
